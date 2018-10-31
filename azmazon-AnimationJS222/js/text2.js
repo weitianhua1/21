4f9b18278c36c5ab1de2cad2682f1bf65073a1d8
weitianhua1 <468592175@qq.com>
2018-10-31 12:20:13 +0800

    1

--- azmazon-AnimationJS222/js/text2.js
+++ azmazon-AnimationJS222/js/text2.js
@@ -9,7 +9,10 @@
 // img0.style.dispaly="none";
 
 var imagesA=document.getElementById("images").children;
-console.log(imagesA);
+var txtList=document.querySelectorAll(".txtItem");
+console.log(txtList);
+
+// console.log(imagesA);
 
 // var img4=document.getElementById('images').children[4];
 // img4.style.display="block";
@@ -20,17 +23,25 @@ console.log(imagesA);
 var currentNo=0;
 
 function changeImg(){
-    for(var i=0;i<imagesA.length;i++){
+    var nodeLength=txt.length
+    for(var i=0;i<nodeLength;i++){
+
         imagesA [i].className="hiddenImg";
         console.log(imagesA[i]);
+        txtList[i].className="txtItem normalColor"
+        
     }
 
     imagesA[currentNo].className="displayImg";
+txtList[currentNo].className="txtItem heighlightColor";
+
     if(currentNo<7) currentNo++;
     else{
         currentNo=0;
     }
-console.log(currentNo);
+// console.log(currentNo);
+
+
 }
 var timer=window.setInterval(changeImg,1000)
 function stopChange (){
@@ -40,7 +51,52 @@ function startChange(){
     timer=window.setInterval(changeImg,1000);
 }
 
-var imagesDiv=document.getElementById("images");
-console.log(imagesDiv);
-imagesDiv.addEventListener('mouseover',stopChange);
-imagesDiv.addEventListener('mouseout',startChange);
+var sliderDiv=document.querySelector(".slider");
+// console.log(imagesDiv);
+sliderDiv.addEventListener('mouseover',stopChange);
+sliderDiv.addEventListener('mouseout',startChange);
+
+for(var i=0;i<txtList.length;i++){
+    txtList[i].addEventListener('mouseover',gotoImg);
+    txtList[i].no=i;
+;    console.log(txtList[i].no);
+}
+function gotoImg(){
+//    console.log(this.no);
+   currentNo=this.no;
+   changeImg();
+    
+}
+var leftButton=document.querySelector('.leftButton');
+var rightButton=document.querySelector('.rightButton');
+
+leftButton.addEventListener('click',leftImg);
+rightButton.addEventListener('click',rightImg)
+
+function leftImg(){
+    for(var i=0;i<nodeLength;i++){
+
+        imagesA [i].className="hiddenImg";
+        // console.log(imagesA[i]);
+        txtList[i].className="txtItem normalColor"
+        
+    }
+
+    if(currentNo>0) {currentNo--;}
+    else{
+        currentNo=7;
+    }
+
+    imagesA [i].className="hiddenImg";
+    // console.log(imagesA[i]);
+    txtList[i].className="txtItem normalColor"
+
+}
+function rightImg(){
+    if(currentNo<7) {currentNo++;}
+    else{
+        currentNo=0;
+    }
+    console.log(currentNo);
+    changeImg();
+}
